c665f9bc23a16f8baf06b1426badcafbbceb0f5e
Connor Small <csmall0471@gmail.com>
2019-12-11 18:27:47 -0700

    Inital Commit

---
+++ WeatherAppV2/WeatherAppV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAppV2
+//
+//  Created by Connor Small on 12/11/19.
+//  Copyright © 2019 Connor Small. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
